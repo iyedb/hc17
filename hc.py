83b41b278f94c7933951bc3df99ed33f83ff5de1
Iyed Bennour <iyed.bennour@gmail.com>
2017-03-01 22:47:28 +0100

    score

--- hc.py
+++ hc.py
@@ -1,9 +1,9 @@
+import sys
 import numpy as np
 from scipy.spatial import distance
 
 
 
-file = open('in.data', 'r')
 
 
 class Video(object):
@@ -65,6 +65,12 @@ class Cache(object):
             arr[k] = v
         return arr
 
+    def dist(self, baselength):
+        return distance.euclidean(self.to_vec(baselength), np.zeros(baselength, dtype=np.int32))
+
+    def videolist(self):
+        return [v.vid for v in self.videos]
+
     def __str__(self):
         s = 'cache %d [' % self.cid
         for k, v in self.endpoints.iteritems():
@@ -89,23 +95,24 @@ def parse(file):
 
     vidcount, eps, reqs, ccount, csize = [int(e) for e in lines[0].split(' ')]
 
-    print 'videos %d' % vidcount
-    print 'endpoints %d' % eps
-    print 'requests %d' % reqs
-    print 'caches %d' % ccount
-    print 'cache size %d' % csize
+    # print 'videos %d' % vidcount
+    # print 'endpoints %d' % eps
+    # print 'requests %d' % reqs
+    # print 'caches %d' % ccount
+    # print 'cache size %d' % csize
 
     videos = []
     vids = lines[1].split(' ')
     for vid, size in enumerate(vids):
         videos.append(Video(vid, int(size)))
 
-    for v in videos:
-        print v
+    # for v in videos:
+    #    print v
 
     line = 2
     endpoints = []
     caches = {}
+    reqs = {}
 
     for ep in range(eps):
         latencyd, cachecount = [int(e) for e in lines[line].split(' ')]
@@ -122,7 +129,6 @@ def parse(file):
 
         line += 1
 
-    
     for i in range(line, len(lines)):
         v, e, r = [int(e) for e in lines[i].split(' ')]
         videos[v].addendpoint(e, r)
@@ -131,20 +137,60 @@ def parse(file):
     return [v for v in videos if v.popularity() != 0], caches, endpoints
 
 
-videos, caches, eps = parse(file)
 
-for k, v in caches.iteritems():
-    print 'cache %d vector:' % v.cid, v.to_vec(len(eps))
 
-videos = sorted(videos, reverse=True)
+# for k, v in caches.iteritems():
+#     print v.cid, v.videolist()
 
-for v in videos:
-    print 'video %d vector:' % v.vid, v.to_vec(len(eps)),\
-            ' %s' % v.popularity()
-for vid in videos:
+if __name__ == '__main__':
+
+    file = open(sys.argv[1], 'r')
+    out = sys.argv[1].split('.')[0]
+    out = out + '.out'
+    print out
+    videos, caches, eps = parse(file)
+
+    # for k, v in caches.iteritems():
+    #     print 'cache %d vector:' % v.cid, v.to_vec(len(eps)), v.dist(len(eps))
+
+    videos = sorted(videos, reverse=True)
+
+    """
+    for v in videos:
+        print 'video %d vector:' % v.vid, v.to_vec(len(eps)),\
+                ' %s' % v.popularity()
+    """
+
+    for video in videos:
+        # print 'Video %d =>' % video.vid
+        distances = []
+        for k, v in caches.iteritems():
+            d = distance.cosine(v.to_vec(len(eps)), video.to_vec(len(eps)))
+            distances.append((v.cid, d, v.dist(len(eps)), v.size))
+            # print 'D(video %d, cache %d)' % (video.vid, v.cid), d
+        # print 'video %d:' % video.vid, sorted(distances, key=lambda x: (x[1], x[2]))
+        distances = sorted(distances, key=lambda x: (x[1], x[2]))
+        for e in distances:
+            if e[1] == 1.0:
+                continue
+            cache_ = caches.get(e[0])
+            if cache_.put(video):
+                break
+
+        # print '----'
+    sol = open(out, 'w')
+    numcaches = 0
+    outcaches = []
     for k, v in caches.iteritems():
-        print 'D(video %d, cache %d)' % (vid.vid, v.cid),\
-                distance.cosine(v.to_vec(len(eps)), vid.to_vec(len(eps)))
-    print '----'
+        if len(v.videolist()):
+            numcaches += 1
+            outcaches.append(v)
+    outcaches = sorted(outcaches, key=lambda x: x.cid)
+    sol.write('%d\n' % numcaches)
 
+    for c in outcaches:
+        sol.write('%d ' % c.cid)
+        for v in c.videolist():
+            sol.write('%d ' % v)
+        sol.write('\n')
 
